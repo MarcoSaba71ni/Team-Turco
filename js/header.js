2e078ffa2b89696fdcb4fd7cb67ae1082f02f6d5
Marco Sabatini <marsab04188@stud.noroff.no>
2025-08-15 09:40:39 +0200

    Add responsive mobile dropdown menu to header
    Introduced a burger menu and dropdown navigation for mobile devices. Updated CSS for responsive design, added new elements in header.html, and implemented basic dropdown toggle functionality in js/header.js.

---
+++ js/header.js
@@ -0,0 +1,6 @@
+const dropdownBtn = document.getElementById("dropdown-btn");
+const dropdown = document.getElementById("dropdown");
+
+dropdownBtn.addEventListener("click", ()=> {
+    dropdown.style.display = "flex";
+})
